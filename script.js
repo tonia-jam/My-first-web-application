4d6b0f3046825bc323ebed31373456cfb5f51c2e
tonia-jam <66325349+tonia-jam@users.noreply.github.com>
2020-06-23 21:21:05 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let data = {
+    picture: 'photos/csillagjazmin.jpg',
+    title: 'Trachelospermum jasminoides - Star jasmine',
+    description: 'Star jasmine is a vine that produces highly fragrant, white blossoms that attract bees. A valuable perfume oil is extracted from the steam distilled or tinctured flowers and used in high end perfumery.'
+  };
+  
+  $('#picture').attr('src', data.picture);
